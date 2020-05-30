f1cf4ba73e104cdd05c9e5fd1f51f8543965f9d5
bayufajariyanto <bayuf08@gmail.com>
2020-05-30 07:35:22 +0700

    bug fixed

--- application/views/member/barang.php
+++ application/views/member/barang.php
@@ -21,7 +21,7 @@ function rupiah($angka)
   <?php foreach($barang as $b) : 
     if($b['kategori'] === $k['nama']) :
     ?>
-    <div class="col-md-4">
+    <div class="col-md-3">
       <div class="card shadow mb-4 rounded">
         <img class="rounded" src="<?= base_url('assets/img/') ?><?= $b['nama'] ?>.png" alt="" width="100%">
         <div class="card-body">
